d8514b32d5152a520a07eaf7b4d764e594c2c564
Imran Bepari <imranbepari97@gmail.com>
2019-03-23 15:23:47 +0000

    Student home layout done

--- app/app/src/main/java/com/comp6239/AdminHomeActivity.java
+++ app/app/src/main/java/com/comp6239/AdminHomeActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 //Home Activity, containing two tabs
 public class AdminHomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,15 +17,16 @@ public class AdminHomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                case R.id.navigation_search_tutors:
+                    //mTextMessage.setText(R.string.title_home);
+                    return true;/*
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    //mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
+                    */
             }
             return false;
         }
@@ -36,7 +37,6 @@ public class AdminHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
--- app/app/src/main/java/com/comp6239/StudentHomeActivity.java
+++ app/app/src/main/java/com/comp6239/StudentHomeActivity.java
@@ -1,15 +1,29 @@
 package com.comp6239;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class StudentHomeActivity extends AppCompatActivity {
+import com.comp6239.dummy.DummyContent;
 
-    private TextView mTextMessage;
+public class StudentHomeActivity extends AppCompatActivity implements StudentMyProfileFragment.OnFragmentInteractionListener, StudentMyTutorsFragment.OnListFragmentInteractionListener, StudentSearchTutorsFragment.OnListFragmentInteractionListener {
+
+
+    //Keeps the Fragments persistent rather than loading and creating them fresh every time
+    //they're selected
+    final Fragment myProfile = new StudentMyProfileFragment();
+    final Fragment searchTutors = new StudentSearchTutorsFragment();
+    final Fragment myTutors = new StudentMyTutorsFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment activeFrag = searchTutors;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +31,20 @@ public class StudentHomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_search_tutors:
+                    Log.i("push button", "searchtutor");
+                    fm.beginTransaction().hide(activeFrag).show(searchTutors).commit();
+                    activeFrag = searchTutors;
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_my_tutors:
+                    Log.i("push button", "mytutors");
+                    fm.beginTransaction().hide(activeFrag).show(myTutors).commit();
+                    activeFrag = myTutors;
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_my_profile:
+                    Log.i("push button", "myprofile");
+                    fm.beginTransaction().hide(activeFrag).show(myProfile).commit();
+                    activeFrag = myProfile;
                     return true;
             }
             return false;
@@ -36,9 +56,22 @@ public class StudentHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        fm.beginTransaction().add(R.id.main_container, myProfile, "3").hide(myProfile).commit();
+        fm.beginTransaction().add(R.id.main_container, myTutors, "2").hide(myTutors).commit();
+        fm.beginTransaction().add(R.id.main_container,searchTutors, "1").commit();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 }
