9a410f5380cfab52ba1204a424a446b16dc2b51a
Huw Jones <huwcbjones@gmail.com>
2019-05-13 17:10:37 +0100

    Added delete subject

--- backend/controllers/subjects.py
+++ backend/controllers/subjects.py
@@ -1,9 +1,14 @@
+from http import HTTPStatus
+
+from sqlalchemy.orm import Session
+
 from backend.controller import Controller
 from backend.database import generate_unique_id
-from backend.exc import UnauthorisedException, ResourceAlreadyExistsException
+from backend.exc import UnauthorisedException, ResourceAlreadyExistsException, NotFoundException
 from backend.models import UserRole, Subject
-from backend.models.subject import get_subjects, subject_exists_by_name, subject_exists_by_id
+from backend.models.subject import get_subjects, subject_exists_by_name, subject_exists_by_id, get_subject_by_id
 from backend.oauth import protected
+from backend.utils.regex import uuid as uuid_regex
 
 
 class AllSubjectsController(Controller):
@@ -36,3 +41,23 @@ class AllSubjectsController(Controller):
             s.add(subject)
             s.commit()
             self.write({"id": subject.id, "name": subject.name})
+
+
+class SubjectController(Controller):
+    route = [
+        r"/subject/(" + uuid_regex + ")"
+    ]
+
+    @protected
+    async def delete(self, subject_id):
+        if self.current_user.role != UserRole.ADMIN:
+            raise UnauthorisedException()
+
+        with self.app.db.session() as s:  # type: Session
+            if not subject_exists_by_id(subject_id, s):
+                raise NotFoundException("A subject with that ID was not found")
+            subject = get_subject_by_id(subject_id, s)
+            s.delete(subject)
+            s.commit()
+
+            self.set_status(HTTPStatus.NO_CONTENT)
